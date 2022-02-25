06f48f1c21b11ceaba7a63c23c827b5d882d8095
Expo <ashique00003@gmail.com>
2022-02-25 19:46:54 +0600

    Testing github logs

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask
+from flask import jsonify
+import git
+import  os
+from pydriller import Repository
 app = Flask(__name__)
 
-
+# def run():
+#     unknown_dir = os.system("git log")
+#     return unknown_dir
 
 @app.route('/test')
-def hello_world():
-    return 'Hello World!'
+
+
+def get_logs_from_github():
+    g = git.Git('https://github.com/MhAiub/flaskProject/')
+    hexshas = g.log()
+    return jsonify(hexshas)
+
 
 
 if __name__ == '__main__':
