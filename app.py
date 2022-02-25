7578210b1b16d9580d102cae3000366dd7d5e137
Muhammad Aiub Alli <83600123+MhAiub@users.noreply.github.com>
2022-02-26 02:31:08 +0600

    Fauture/info (#3)
    * New file added
    
    * New file by mee
    
    * Testing github logs
    
    * Test
    
    * Tst
    
    * Web scrab
    
    Co-authored-by: Expo <ashique00003@gmail.com>

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
-from flask import Flask
-from flask import jsonify
-import git
+import os
+import selenium
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import pandas as pd
+import xlwt
 
-app = Flask(__name__)
+# data = soup.find('a', {'class':"block-link__overlay-link"})
 
+driver = webdriver.Chrome("/home/expo/.wdm/drivers/chromedriver/linux64/96.0.4664.45/chromedriver")
 
-@app.route('/test')
-def get_logs_from_github():
-    repo = git.Git('https://github.com/MhAiub/flaskProject/')
-    logs = repo.log()
-    return jsonify(logs), 200
+base_url = 'https://www.bbc.com/'
+driver.get(base_url)
+html_source = driver.page_source
+soup = BeautifulSoup(html_source, 'html.parser')
 
+all_data = [data['href'] for data in soup.find_all('a', {'class': "block-link__overlay-link"})]
 
-if __name__ == '__main__':
-    app.run()
+for link in all_data:
+    link_url = 'https://www.bbc.com'
+    query = link_url+link
+    driver.get("https://www.bbc.com/future/article/20220223-the-chemical-that-sharpens-our-social-skills")
+    html_source = driver.page_source
+    soup = BeautifulSoup(html_source, 'html.parser')
+    data = soup.find_all('p', {'class': "Normal1"})
+    print(data)
+
+# excel_data = {'Link': all_data, 'Name': 'BBC WEB LINKS'}
+# df = pd.DataFrame(excel_data, columns=['Link', 'Name'])
+# df.to_excel(r'/home/expo/Documents/bbc_wesite_links.xls', index=False, header=True)
+
--- test.py
+++ test.py
@@ -1,3 +1,33 @@
+import os
+import selenium
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+from PIL import Image
+import io
+import requests
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import ElementClickInterceptedException
+
+
+driver = webdriver.Chrome("/home/expo/.wdm/drivers/chromedriver/linux64/96.0.4664.45/chromedriver")
+
+search_url='https://www.bbc.com/'
+html_source = driver.page_source
+driver.get(search_url)
+import pdb;pdb.set_trace()
+
+
+#Scroll to the end of the page
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+time.sleep(5)#sleep_between_interactions
+imgResults = driver.find_elements_by_xpath("/block-link__overlay-link")
+totalResults=len(imgResults)
+print(driver)
+print(len(imgResults))
+
+
+
 import glob
 import os
 import zlib
